57efdadb6840488dec8be6f284be43c5c1c182f5
DongWoo Son <99868561+dnd-qodqks@users.noreply.github.com>
2023-03-18 00:56:05 +0900

    Update Install_vscode_ros2_foxy_gazebo.sh

--- Install_vscode_ros2_foxy_gazebo.sh
+++ Install_vscode_ros2_foxy_gazebo.sh
@@ -4,15 +4,12 @@
 set -x
 
 echo "[Install VS Code]"
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-sudo apt install apt-transport-https
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
-sudo apt-get install git
 
 # Delete Cache
 sudo rm /var/lib/apt/lists/lock
@@ -156,4 +153,3 @@ sudo apt install ros-foxy-gazebo-ros-pkgs -y
 echo "[Complete!!!]"
 exec bash
 exit 0
-
